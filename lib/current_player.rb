de8a530429bae78a1b014c9900d7e8f262fb926d
Jake G <jake098877@gmail.com>
2020-04-19 18:18:39 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,9 +1,18 @@
 def turn_count(boards)
   count = 0
-boards.each do |board|
-  if (board == "X" || board == "O")
-  count += 1
+  boards.each do |board|
+    if (board == "X" || board == "O")
+    count += 1
+  end
+  end
+  count
 end
-end
-count
+
+
+def current_player
+  if(count % 2 == 0)
+    puts "X"
+  else
+    puts "O"
+  end
 end
